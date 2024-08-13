82902444885d38e3c774680c547d444a3fa06c7c
RonenGupta <ronen.gupta@education.nsw.gov.au>
2024-08-13 15:07:40 +1000

    Plotting Stuff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 df = pd.read_csv('cars_2010_2020.csv')
 
 def Sorting_Make():
@@ -20,5 +20,23 @@ Sorting_Model()
 
 print(df)
 
+#def outputting_model():
+   # global plt
 
+df.plot(
+    kind = 'bar',
+    x ='Make',
+    y='Price (USD)',
+    color='blue',
+    alpha=0.3,
+    title="Plot"
+    )
+
+plt.show()
+
+#print(df['Price (USD)'].mean())
+#print(df['Model'].value_counts())
+
+
+#print(df.describe())
 

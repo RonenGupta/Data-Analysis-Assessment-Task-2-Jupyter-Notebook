6443bf4de0b67974ad98bd0f33496f046b5d636a
RonenGupta <160193990+RonenGupta@users.noreply.github.com>
2024-08-14 19:33:38 +1000

    Fixes
    Deleted Average_Make.csv, Average_Model.csv already provided enough information. All of the matplotlib stays the same, I require that for graphing and for inserting into the GUI. I also made the dataframe for average_make a lot more simpler, by deleting other columns because they made extra car types with those extra columns. I made it only make and average price, so I could easily find what was the most expensive and cheapest car.

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('cars_2010_2020.csv')
 
 # Calculate the average price for each make, take the columns from the original csv file
-average_make = df.groupby(['Make', 'Year', 'Engine Size (L)', 'Fuel Type'])['Price (USD)'].mean().reset_index()
-average_make.columns = ['Make', 'Year', 'Engine Size (L)', 'Fuel Type', 'Average_Price']
-print(average_make)
+average_make = df.groupby(['Make'])['Price (USD)'].mean().reset_index()
+average_make.columns = ['Make', 'Average_Price']
 
+# Round to 2 decimal places
 average_make['Average_Price'] = average_make['Average_Price'].round(2)
 print(average_make)
 
-# Calculate the average price for each model, take the columns from the original csv file
+# Calculate the price for each model, take the columns from the original csv file
 average_model = df.groupby(['Make', 'Model', 'Year', 'Engine Size (L)', 'Fuel Type'])['Price (USD)'].mean().reset_index()
-average_model.columns = ['Make', 'Model', 'Year', 'Engine Size (L)', 'Fuel Type', 'Average_Price']
-print(average_model)
+average_model.columns = ['Make', 'Model', 'Year', 'Engine Size (L)', 'Fuel Type', 'Price']
 
-average_model['Average_Price'] = average_model['Average_Price'].round(2)
+# Round to 2 decimal places
+average_model['Price'] = average_model['Price'].round(2)
 print(average_model)
 
 #Do the same, but for plotting (Other DataFrame Holds Too Much Information Due to Extra Columns)
@@ -52,5 +52,5 @@ average_modelplot.plot(
 
 plt.show()
 
-average_make.to_csv('Average_Make.csv', index=False)
+# Storing only model because we need each individual car with all attributes. The other dataframe is simply for the graph and for finding what is the cheapest average make.
 average_model.to_csv('Average_Model.csv', index=False)
